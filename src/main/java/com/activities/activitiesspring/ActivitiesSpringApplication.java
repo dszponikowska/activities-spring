f00373caac859b0ff0bd22e1725b84f967c360c3
dszponikowska <szponikowska.dorota@gmail.com>
2021-05-31 13:13:38 +0200

    Add ActivitiesSpringApplication.java class to git

---
+++ src/main/java/com/activities/activitiesspring/ActivitiesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.activities.activitiesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiesSpringApplication.class, args);
+    }
+
+}
